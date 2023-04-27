3a7b7984255ce180f51840061e9d5a2a88437848
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-04-27 13:59:01 +0200

    css adaptive

--- js/menu.js
+++ js/menu.js
@@ -6,8 +6,10 @@
     };
     refs.openMenuBtn.addEventListener('click', toggleMenu);
     refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
     function toggleMenu() {
         refs.menu.classList.toggle('is-hidden');
         document.body.classList.toggle('no-scroll');
     }
 })();
+
